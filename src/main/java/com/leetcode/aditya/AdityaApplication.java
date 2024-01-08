0af19cf69d03da1db8903307c55963031901884d
Aditya Gamini <gamini.aditya@leapfinance.com>
2024-01-08 15:34:42 +0530

    Creating project

---
+++ src/main/java/com/leetcode/aditya/AdityaApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode.aditya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdityaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdityaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leetcode/aditya/AdityaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.aditya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdityaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
